2d04d382de8d86f9e3ac6a8b33a2694b015e8caa
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-08 23:50:21 -0500

    First commit

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-  constructor(amount) {
-    this.amount = amount;
+  get balance() {
+    let balance = 0;
+    for (let transaction of this.transactions){
+      balance += transaction.value;
+    }
+    return balance;
+
+  }
+
+  addTransaction(transaction){
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    
+    if (this.isAllowed()){
+      this.time = new Date();
+      this.account.addTransaction(this);
+      return true;
+    }
+
+    return false;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed(){
+    if (this.amount < 0){
+      return false;
+    }
+    return true;
+  }
+
+}
+
+class Withdrawal extends Transaction{
+
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed(){
+    if (this.account.balance < this.amount){
+      return false;
+    }
+    return true;
   }
 
 }
@@ -18,12 +76,27 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
+const myAccount = new Account("snow-patrol");
+
+t1 = new Withdrawal(50.25, myAccount);
+console.assert(t1.commit(), "Withdrawal failed!");
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
+t2 = new Withdrawal(9.99, myAccount);
+console.assert(t1.commit(), "Withdrawal failed!");
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+t4 = new Withdrawal(5000, myAccount);
+console.assert(t4.commit(), "Withdrawal failed!");
+console.log('Transaction 4:', t4);
+
+t5 = new Withdrawal(20, myAccount);
+console.assert(t5.commit(), "Withdrawal failed!");
+console.log('Transaction 5:', t5);
+
+console.log('Balance:', myAccount.balance);
+console.log(myAccount.transactions);
